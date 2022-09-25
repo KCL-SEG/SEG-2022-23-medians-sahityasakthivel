ebd0b7f819af2a03bef2251a93bed5845ea2ff10
Sahitya Sakthivel <sahitya.sakthivel@kcl.ac.uk>
2022-09-25 14:04:27 +0100

    Added my solution for this exercise.

--- median.py
+++ median.py
@@ -1,6 +1,7 @@
+import math
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
-
+""
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
@@ -9,4 +10,11 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
+numbers.sort()
 print(numbers)
+
+if(len(numbers)% 2 == 0):
+    value = int(len(numbers) / 2)
+    print((numbers[value] + numbers[value -1])/2)
+else:
+     print(numbers[math.floor(len(numbers) / 2)])
